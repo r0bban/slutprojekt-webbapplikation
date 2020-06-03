a751ebfeed8c1ddccb5f27c4450a4c758521f8b6
r0bban <robert.p.svensson@gmail.com>
2020-06-03 16:35:32 +0200

    api created, getProducts and createProduct working

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,31 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import API from "@/api/server";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    tmpData: ["Champinjon", "Räkor", "Krabba", "Kräfta", "Ostron", "Musslor"],
+    products: [],
+    cart: [],
+    currentUser: "",
+    userToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlcyNUFZR0U0Z2MxZ1hmMGQiLCJleHAiOjE1OTExOTU4NzksImlhdCI6MTU5MTE5MjI3OX0.1hD8y3ACV8uREIn7HYgxTGkiM6xDwnDmhqGH4A229e0",
     showCart: false,
     showLogin: false,
+    newProduct: {
+      "title": "Johannes produkt",
+      "price": 123,
+      "shortDesc": "Unisex",
+      "longDesc": "kalle NY ipsum dolor sit amet, 50-50 Sidewalk Surfer nose bump kickflip bruised heel fakie berm soul skate. Bluntslide transition nollie hard flip bank pressure flip ho-ho. Steps rip grip nosepicker roll-in yeah 540 pump. ",
+      "imgFile": "kalle NY skateboard-generic.png",
+  },
+    updatedProduct: {
+      "title": "uppdtaerad produkt",
+      "price": 123,
+      "shortDesc": "Unisex",
+      "longDesc": "kalle uppdaterad ipsum dolor sit amet, 50-50 Sidewalk Surfer nose bump kickflip bruised heel fakie berm soul skate. Bluntslide transition nollie hard flip bank pressure flip ho-ho. Steps rip grip nosepicker roll-in yeah 540 pump. ",
+      "imgFile": "kalle uppdaterad skateboard-generic.png",
+  },
   },
   mutations: {
     toggleCart(state) {
@@ -16,8 +34,29 @@ export default new Vuex.Store({
     toggleLogin(state) {
       state.showLogin = !state.showLogin;
     },
+    setStoreProducts(state, newProductList) {
+      state.products = newProductList;
+    },
+    addProductToStoreProducts(state, newProduct){
+      state.products.push(newProduct)
+    }
   },
   getters: {},
-  actions: {},
+  actions: {
+    async initialProductLoad(context) {
+      if (context.state.products.length <= 0) {
+        await context.dispatch('refreshProducts')
+      }
+    },
+    async refreshProducts(context) {
+      const newProductList = await API.fetchProducts(context.state.userToken);
+      context.commit("setStoreProducts", newProductList);
+    },
+    async registerNewProduct(context, newProduct){
+      const addedProduct = await API.postProductRequest(newProduct, context.state.userToken);
+      context.commit("addProductToStoreProducts", addedProduct);
+    }
+  },
   modules: {},
 });
+
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="products">
-    <ul>
+    <button v-on:click="initialProductLoad">hämta produkter</button>
+    <button v-on:click="addProduct">lägg till produkter</button>
+    <div
+    v-for="(product, index) in this.$store.state.products"
+    :key="index"
+    ></div>
+    <!-- <ul>
       <li v-for="(product,index) in this.$store.state.tmpData" :key="index">
         <router-link :to="{name: 'Product', params: {id: index+1}}">Show {{product}} Big</router-link>
       </li>
     </ul>
     <hr />
     <router-link :to="{name: 'Products'}">Go Back to All</router-link>
-    <router-view />
+    <router-view /> -->
   </div>
 </template>
 <script>
@@ -16,6 +22,14 @@ export default {
     return {
       showProduct: false
     };
+  },
+  methods: {
+    initialProductLoad() {
+      this.$store.dispatch("initialProductLoad");
+    },
+    addProduct() {
+      this.$store.dispatch("registerNewProduct", this.$store.state.newProduct);
+    },
   }
 };
 </script>
